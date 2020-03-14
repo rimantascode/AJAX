750e155951da8d0b984b548bf732ebe798a8a291
rimantascode <r.preiks@gmail.com>
2020-03-14 12:46:14 +0000

    Getting the data on the page

--- ajax-test/main.js
+++ ajax-test/main.js
@@ -1,17 +1,20 @@
-function getData (cb){
-var xhr=new XMLHttpRequest();
+const baseURL = "https://swapi.co/api/";
 
+function getData(type, cb) {
+    var xhr = new XMLHttpRequest();
 
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            cb(JSON.parse(this.responseText));
+        }
+    };
 
-xhr.open("GET","https://swapi.co/api/");
-xhr.send();
-
-xhr.onreadystatechange = function () {
-    if (this.readyState==4 && this.status==200) {
-        cb(JSON.parse(this.responseText));
-    }    
-};
+    xhr.open("GET", baseURL + type + "/");
+    xhr.send();
 }
-getData(function(data){
-    console.log(data);
-});
+
+function writeToDocument(type) {
+    getData(type, function(data) {
+        document.getElementById("data").innerHTML = data;
+    });
+}
