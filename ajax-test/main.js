613d41349bc0bd4238f45b851b04472489e0386f
rimantascode <r.preiks@gmail.com>
2020-03-14 12:24:27 +0000

    callbacks

--- ajax-test/main.js
+++ ajax-test/main.js
@@ -1,19 +1,17 @@
+function getData (cb){
 var xhr=new XMLHttpRequest();
-var data;
+
 
 
 xhr.open("GET","https://swapi.co/api/");
 xhr.send();
-function setData(jsonData){
-    data = jsonData;
-    
-}
 
 xhr.onreadystatechange = function () {
     if (this.readyState==4 && this.status==200) {
-        data = JSON.parse(this.responseText);
+        cb(JSON.parse(this.responseText));
     }    
 };
-setTimeout(function(){
+}
+getData(function(data){
     console.log(data);
-},800);
+});
