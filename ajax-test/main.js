418cde14f0fb98e6b917cd430c291a50a6e4074d
rimantascode <r.preiks@gmail.com>
2020-03-14 15:17:13 +0000

    Tabular Data - Part one

--- ajax-test/main.js
+++ ajax-test/main.js
@@ -1,5 +1,3 @@
-
-
 const baseURL = "https://swapi.co/api/";
 
 function getData(type, cb) {
@@ -15,15 +13,28 @@ function getData(type, cb) {
     xhr.send();
 }
 
+function getTableHeaders(obj) {
+    var tableHeaders = [];
+
+    Object.keys(obj).forEach(function(key) {
+        tableHeaders.push(`<td>${key}</td>`)
+    });
+
+    return `<tr>${tableHeaders}</tr>`;
+}
+
 function writeToDocument(type) {
     var el = document.getElementById("data");
     el.innerHTML = "";
 
     getData(type, function(data) {
         data = data.results;
+        var tableHeaders = getTableHeaders(data[0]);
 
         data.forEach(function(item) {
-            el.innerHTML += "<p>" + item.name + "</p>";
+            // el.innerHTML += "<p>" + item.name + "</p>";
         });
+
+        el.innerHTML = `<table>${tableHeaders}</table>`;
     });
 }
